2b75217067bb4afe51e4ba8f5c891522ee09e0bd
sanebou nogino <nogino.sanebou@gmail.com>
2026-06-05 16:05:23 +0900

    println(1 + 2 + 3)などの2以上連続した加算処理に対応

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+#[derive(Clone)]
 enum Expr {
-    Value(Value),
-    Func(StdFunc),
     Expr(Box<Expr>),
+    Value(Value),
+    Func(BuiltinFunc),
+    Binary(Binary),
 }
 
 impl Expr {
@@ -15,22 +17,22 @@ impl Expr {
                     Value::Number(num) => {
                         Ok(Value::Number(*num))
                     },
-                    Value::Boolean(_) => {
+                    _ => {
                         panic!("想定外のvalue-Expression")
-                    },
-                    Value::Formula(formula) => {
-                        Ok(formula.calc())
                     },
                 }
             },
+            Expr::Binary(bin) => {
+                Ok(bin.calc())
+            },
             Expr::Func(func) => {
                 match func {
-                    StdFunc::Print(expr) => {
+                    BuiltinFunc::Print(expr) => {
                         let r = expr.eval();
                         let r = print(r.expect("printのexprに失敗"));
                         Ok(r)
                     },
-                    StdFunc::Println(expr) => {
+                    BuiltinFunc::Println(expr) => {
                         let r = expr.eval();
                         let r = println(r.expect("printlnのexprに失敗"));
                         Ok(r)
@@ -41,20 +43,21 @@ impl Expr {
     }
 }
 
-enum StdFunc {
+#[derive(Clone)]
+enum BuiltinFunc {
     Print(Box<Expr>),
     Println(Box<Expr>),
 }
 
 #[derive(Clone)]
-struct Formula {
-    lhs: Box<Value>,
-    rhs: Box<Value>,
+struct Binary {
+    lhs: Box<Expr>,
+    rhs: Box<Expr>,
     op: Op,
 }
 
-impl Formula {
-    fn new(lhs: Box<Value>, rhs: Box<Value>, op: Op) -> Self {
+impl Binary {
+    fn new(lhs: Box<Expr>, rhs: Box<Expr>, op: Op) -> Self {
         Self {
             lhs,
             rhs,
@@ -63,50 +66,27 @@ impl Formula {
     }
 
     fn calc(&self) -> Value {
-        let lhs = match &*self.lhs {
-            Value::Formula(f) => {
-                f.calc()
-            },
-            Value::Number(_) => {
-                *self.lhs.clone()
-            },
-            Value::Boolean(_) => {
-                panic!("無効なValueです。-calc lhs")
-            },
-        };
-
-        let rhs = match &*self.rhs {
-            Value::Formula(f) => {
-                f.calc()
-            },
-            Value::Number(_) => {
-                *self.rhs.clone()
-            },
-            Value::Boolean(_) => {
-                panic!("無効なValueです。-calc rhs")
-            },
-        };
+        let lhs = self.lhs.eval().expect("calcでlhsの展開に失敗");
+        let rhs = self.rhs.eval().expect("calcでrhsの展開に失敗");
 
         match &self.op {
             Op::Add => {
-                Formula::add(lhs, rhs)
+                Binary::add(lhs, rhs)
             },
         }
     }
 
     fn add(lhs: Value, rhs: Value) -> Value {
-        let lhs = match lhs {
-            Value::Number(num) => {
-                num
-            },
-            _ => panic!("想定外のvalue -lhs")
+        let lhs = if let Value::Number(num) = lhs {
+            num
+        } else {
+            panic!("想定外のvalue -lhs")
         };
 
-        let rhs = match rhs {
-            Value::Number(num) => {
-                num
-            },
-            _ => panic!("想定外のvalue -rhs")
+        let rhs = if let Value::Number(num) = rhs {
+            num
+        } else {
+            panic!("想定外のvalue -rhs")
         };
 
         Value::Number(lhs + rhs)
@@ -120,16 +100,26 @@ enum Op {
 
 #[derive(Clone)]
 enum Value {
+    Unit,
     Number(u64),
     Boolean(bool),
-    Formula(Formula),
+}
+
+#[derive(Clone)]
+enum Token {
+    Text(String),
+    Number(u64),
+    LParen,
+    RParen,
+    Plus,
+    Semicolon,
 }
 
 fn print(value: Value) -> Value {
     match value {
         Value::Number(num) => {
             print!("{}", num);
-            Value::Boolean(true)
+            Value::Unit
         },
         _ => {
             panic!("想定外のvalue-print")
@@ -138,43 +128,41 @@ fn print(value: Value) -> Value {
 }
 
 fn println(value: Value) -> Value {
-    let p = print(value);
-    match p {
-        Value::Boolean(bool) => {
-            if !bool {
-                panic!("printに失敗しています。")
-            }
-        },
-        _ => {
-            panic!("想定外のvalue-println")
-        }
-    }
-
+    print(value);
     println!();
 
-    Value::Boolean(true)
+    Value::Unit
 }
 
 fn main() {
 
 }
 
-fn lexer(code: &str) -> Vec<String> {
+fn lexer(code: &str) -> Vec<Token> {
     let mut token = String::new();
     let mut tokens = Vec::new();
 
     for c in code.chars() {
         match c {
-            '(' | ')' => {
-                tokens.push(token.clone());
+            '(' => {
+                tokens.push(convert_literal(&token));
+                tokens.push(Token::LParen);
+                token.clear();
+            },
+            ')' => {
+                tokens.push(convert_literal(&token));
+                tokens.push(Token::RParen);
                 token.clear();
             },
             '+' => {
-                tokens.push(token.clone());
-                tokens.push(String::from("+"));
+                tokens.push(convert_literal(&token));
+                tokens.push(Token::Plus);
                 token.clear();
             }
-            ' ' | ';' => {
+            ';' => {
+                tokens.push(Token::Semicolon);
+            }
+            ' ' => {
                 continue;
             }
             _ => {
@@ -186,49 +174,96 @@ fn lexer(code: &str) -> Vec<String> {
     tokens
 }
 
-fn parse(tokens: Vec<String>) -> Expr {
-    let (token, tokens) = tokens.split_first().expect("split_firstに失敗");
+fn convert_literal(token: &String) -> Token {
+    if let Ok(num) = token.parse::<u64>() {
+        Token::Number(num)
+    } else {
+        Token::Text(token.clone())
+    }
+}
 
-    match token.as_str() {
-        "print" => {
-            let expr = parse(tokens.to_vec());
-            let func = StdFunc::Print(Box::new(expr));
+fn parse(tokens: Vec<Token>) -> Expr {
+    let mut left= None;
+    let mut tokens = tokens.to_vec();
 
-            return Expr::Func(func);
-        },
-        "println" => {
-            let expr = parse(tokens.to_vec());
-            let func = StdFunc::Println(Box::new(expr));
+    while let (token, mut tokens2) = tokens.split_first().expect("split_firstに失敗") {
+        match token {
+            Token::Text(text) => {
+                if !is_func(text) {
+                    panic!("現在は関数名以外認めていません")
+                }
 
-            return Expr::Func(func);
-        },
-        _ => {
-            if let Ok(num) = token.parse::<u64>() {
-                let Some(next) = tokens.get(0) else {
-                    return Expr::Value(Value::Number(num));
+                let expr = parse(tokens2.to_vec());
+                let func = Expr::Func(parse_func(text, expr));
+                return func;
+            }
+            Token::Number(num) => {
+                left = Some(Expr::Value(Value::Number(*num)));
+            }
+            Token::Plus => {
+                let left2 = if let Some(expr) = left {
+                    expr
+                } else {
+                    panic!("+の前が存在しなかった")
                 };
 
-                if next.as_str() == "+" {
-                    let lhs = Box::new(Value::Number(num));
-
-                    let next = tokens.get(1).expect("想定外:parse:tokens.get(0)");
-                    let Ok(num) = next.parse::<u64>() else {
-                        let x = format!("想定外の右辺です。{}", next);
-                        panic!("{}", x)
+                let right = if let Some(num) = tokens2.get(0) {
+                    let num = if let Token::Number(num) = num {
+                        Value::Number(*num)
+                    } else {
+                        panic!("+の次が数字でなかった")
                     };
-                    let rhs = Box::new(Value::Number(num));
-
-                    let f = Formula::new(lhs, rhs, Op::Add);
-
-                    return Expr::Value(Value::Formula(f));
+                    Expr::Value(num)
                 } else {
-                    panic!("構文が想定外です-parse:tokens.get(0)")
-                }
+                    panic!("+の次が数字でなかった2")
+                };
+
+                let bin = Binary::new(Box::new(left2), Box::new(right), Op::Add);
+                let expr = Expr::Binary(bin);
+
+                left = Some(expr);
+
+                tokens2 = &tokens2[1..];
+            }
+            _ => {
             }
         }
+
+        if tokens2.is_empty() {
+            break;
+        }
+
+        tokens = tokens2.to_vec();
     }
 
-    panic!("構文が想定外です");
+    if let Some(expr) = left {
+        expr
+    } else {
+        panic!("構文が想定外です。")
+    }
+}
+
+fn is_func(name: &str) -> bool {
+    match name {
+        "print" | "println" => {
+            true
+        },
+        _ => false
+    }
+}
+
+fn parse_func(name: &str, args: Expr) -> BuiltinFunc {
+    match name {
+        "print" => {
+            BuiltinFunc::Print(Box::new(args))
+        },
+        "println" => {
+            BuiltinFunc::Println(Box::new(args))
+        },
+        _ => {
+            panic!("{}", format!("存在しない関数名です。{}", name))
+        },
+    }
 }
 
 fn eval(expr: Expr) -> anyhow::Result<Value> {
@@ -246,8 +281,7 @@ mod tests {
 
         if let Ok(result) = eval(expr) {
             match result {
-                Value::Boolean(bool) => {
-                    assert!(bool)
+                Value::Unit => {
                 },
                 _ => unreachable!(),
             }
@@ -256,13 +290,26 @@ mod tests {
 
     #[test]
     fn test2() {
-        let tokens = lexer("print(1 + 2);");
+        let tokens = lexer("println(3 + 2);");
         let expr = parse(tokens);
 
         if let Ok(result) = eval(expr) {
             match result {
-                Value::Boolean(bool) => {
-                    assert!(bool)
+                Value::Unit => {
+                },
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    #[test]
+    fn test3() {
+        let tokens = lexer("print(1 + 2 + 5);");
+        let expr = parse(tokens);
+
+        if let Ok(result) = eval(expr) {
+            match result {
+                Value::Unit => {
                 },
                 _ => unreachable!(),
             }
